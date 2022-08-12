85b1940cd8e74448a6df564e9cd2e70420d56094
Ali Burak <72763348+ali019283@users.noreply.github.com>
2022-08-13 01:28:25 +0300

    Delete include directory

--- include/json.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <json-c/json.h>
-
-/* Check whether the specified object is valid JSON. */
-int is_valid_json(struct json_object *object);
-
-/* Get the type of a packet. */
-int get_packet_type(struct json_object *object);
--- include/util.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-/* Remove all newline characters. */
-void strip_newline(char *str);
-
-/* Generate a random number. */
-int random_num(int min, int max);
